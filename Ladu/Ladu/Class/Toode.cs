df57b37f6e00596e7995f81be547ad7c7a1e7fda
Artur Shabunov <artixd@yandex.ru>
2018-05-03 11:10:42 +0300

    add select and change Toode

--- Ladu/Ladu/Class/Toode.cs
+++ Ladu/Ladu/Class/Toode.cs
@@ -11,7 +11,7 @@ namespace Ladu
         public long ID { get; set; }
         public String Nimi { get; set; }
         public long KategoriaFK { get; set; }
-        public Kategooria kategooria { get; set; }
+        public Alamkategooria kategooria { get; set; }
         public int Kogus { get; set; }
         public String KoodToode { get; set; }
         public double Hind { get; set; }
--- Ladu/Ladu/SQL/Select.cs
+++ Ladu/Ladu/SQL/Select.cs
@@ -3,11 +3,63 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.OleDb;
 
 namespace Ladu
 {
     class Select
     {
+        public static List<Klient> GetKlients()
+        {
+            List<Klient> klients = null;
+            using (OleDbConnection conn = ConnectDB.GetConnection())
+            {
+                conn.Open();
+                string sql = "SELECT * FROM klient";
+                OleDbCommand cmd = new OleDbCommand(sql, conn);
+                OleDbDataReader reader = cmd.ExecuteReader();
+                klients = new List<Klient>();
+                using (reader)
+                {
+                    while (reader.Read())
+                    {
+                        Klient klient = new Klient();
+                        klient.ID = (long)reader[0];
+                        klient.Nimi = reader[1].ToString();
+                        klient.Perekonnanimi = reader[2].ToString();
+                        klient.Telefon = reader[3].ToString();
+                        klient.Aadress = reader[4].ToString();
+                        klient.Email = reader[5].ToString();
+                    }
+                }
+            }
+            return klients;
+        }
 
+        public static List<Toode> GetToodes()
+        {
+            List<Toode> toodes = null;
+            using (OleDbConnection conn = ConnectDB.GetConnection())
+            {
+                conn.Open();
+                string sql = "SELECT * FROM toode";
+                OleDbCommand cmd = new OleDbCommand(sql, conn);
+                OleDbCommand cmdKategoria = new OleDbCommand("SELECT * FROM Alamkategooria");
+                OleDbDataReader reader = cmd.ExecuteReader();
+                //
+                toodes = new List<Toode>();
+                using (reader)
+                {
+                    while (reader.Read())
+                    {
+                        Toode toode = new Toode();
+                        toode.ID = (long)reader[0];
+                        toode.Nimi = reader[1].ToString();
+                        //
+                    }
+                }
+            }
+                return toodes;
+        }
     }
 }
